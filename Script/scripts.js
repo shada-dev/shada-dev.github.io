12e82dcbdf223ee0c07443ab129039f5c8f6f77a
shada-dev <146721603+shada-dev@users.noreply.github.com>
2023-10-30 11:27:38 +0530

    Add files via upload

--- Script/scripts.js
+++ Script/scripts.js
@@ -1,15 +1,9 @@
+const marquee = document.getElementById('myMarquee');
 
-function startTicker() {
-  const newsTicker = document.querySelector('.news-ticker ul');
-  const newsItems = document.querySelectorAll('.news-ticker li');
-  let currentIndex = 0;
+        marquee.addEventListener('mouseover', () => {
+            marquee.stop();
+        });
 
-  setInterval(() => {
-      newsItems[currentIndex].style.opacity = '0';
-      currentIndex = (currentIndex + 1) % newsItems.length;
-      newsItems[currentIndex].style.opacity = '1';
-  }, 500); // Adjust the duration (in milliseconds) for each news item
-}
-
-// Start the news ticker when the page loads
-window.addEventListener('load', startTicker);
+        marquee.addEventListener('mouseout', () => {
+            marquee.start();
+        });
