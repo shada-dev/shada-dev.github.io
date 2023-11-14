501bd789a8fa380211e7739d98b190c19ff167f3
shada-dev <146721603+shada-dev@users.noreply.github.com>
2023-11-14 21:08:58 +0530

    Delete Script directory

--- Script/scripts.js
+++
@@ -1,9 +0,0 @@
-const marquee = document.getElementById('myMarquee');
-
-        marquee.addEventListener('mouseover', () => {
-            marquee.stop();
-        });
-
-        marquee.addEventListener('mouseout', () => {
-            marquee.start();
-        });
